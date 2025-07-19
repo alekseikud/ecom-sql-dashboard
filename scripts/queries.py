4636c9469dfa10bc729dac539d975254dac9142b
Aliaksei Kudzelka <aleksejkudelka4@gmail.com>
2025-07-19 13:43:29 +0200

    Added python functions creating csv reports using already created sql functions

--- scripts/queries.py
+++ scripts/queries.py
@@ -1,5 +1,10 @@
 from psycopg2.extensions import connection as _Connection
+from psycopg2._psycopg import Column
 from tabulate import tabulate
+from typing import Any
+from datetime import datetime
+import csv
+
 from scripts.setup_db import server_connect,server_disconnect
 
 def print_all_tables()->None:
@@ -21,4 +26,74 @@ def print_all_tables()->None:
         raise Exception(f"Something happend during printing.Error:{_ex}")
     finally:
         cursor.close()
+        server_disconnect(connection)
+
+#----------FUNCTION FOR  WRITING REPORTS TO CSVs ------------
+def select_to_csv(csv_name:str,rows:list[tuple[Any,...]],header:tuple[Column,...])->None:
+    try:
+        # os.system(f"touch reports/{csv_name}.csv") Do not need since opening automatically create a file
+        with open(f"reports/{csv_name}.csv",mode ="w",encoding="UTF-8") as file:
+            writer=csv.writer(file)
+            writer.writerow([itr[0] for itr in header])
+            writer.writerows(rows)
+    except Exception as _er:
+        raise RuntimeError(f"""Failed writing reports/{csv_name}.csv:\nError:{_er}""")
+
+#----------FUNCTIONS FOR REVENUE DISPLAYMENT------------
+def get_revenue_csv()->None:
+    connection:_Connection=server_connect()#type:ignore
+    try:
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT * FROM get_monthly_revenue()")
+            rows=cursor.fetchall()
+            if rows and cursor.description:
+                select_to_csv("monthly_revenue_report",rows,cursor.description)
+            cursor.execute("SELECT * FROM get_total_revenue()")
+            rows=cursor.fetchall()
+            if rows and cursor.description:
+                select_to_csv("total_revenue_report",rows,cursor.description)
+    except Exception as _ex :
+        raise RuntimeError(f"Failed making report. Error:{_ex}")
+    finally:
+        server_disconnect(connection)
+
+#----------FUNCTION FOR Monthly Category Sales Performance------------
+def get_sales_performance_csv()->None:
+    connection:_Connection=server_connect()#type:ignore
+    try:
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT * FROM sales_performance()")
+            rows=cursor.fetchall()
+            if rows and cursor.description:
+                select_to_csv("monthly_category_sales_performance_report",rows,cursor.description)
+    except Exception as _ex :
+        raise RuntimeError(f"Failed making report. Error:{_ex}")
+    finally:
+        server_disconnect(connection)
+#----------Helper function to find day of the first purchase----------
+def get_first_date()->datetime|None:
+    connection:_Connection=server_connect()#type:ignore
+    try:
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT * FROM first_sale_date()")
+            return (cursor.fetchall())[0][0]
+    except Exception as _ex:
+        raise RuntimeError(f"Error ocurred during finding first date.\nError:{_ex}")
+
+#------------FUNCTION New vs. Returning Customer Analysis------------
+def customer_retention_csv(start_date:datetime|None=None,end_date:datetime=datetime.now())->None:
+    connection:_Connection=server_connect()#type:ignore
+    try:
+        with connection.cursor() as cursor:
+            if start_date==None:
+                start:datetime|None=get_first_date()
+                if start:
+                    start_date=datetime(start.year,start.month,start.day)
+            cursor.execute(f"SELECT * FROM customer_retention('{str(start_date.date())}', '{str(end_date.date())}')")
+            rows=cursor.fetchall()
+            if rows and cursor.description:
+                select_to_csv("customer_retention",rows,cursor.description)
+    except Exception as _ex :
+        raise RuntimeError(f"Failed making report. Error:{_ex}")
+    finally:
         server_disconnect(connection)
